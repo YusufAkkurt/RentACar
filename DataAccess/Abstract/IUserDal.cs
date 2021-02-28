590c8f887e5131b745e08bbbc76a37cba2bad8b6
YusufAkkurt <ysfakkurt@hotmail.com>
2021-02-28 16:08:13 +0300

    User için GetClaims operasyonları yazıldı

--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -1,12 +1,11 @@
 ﻿using Core.DataAccess;
 using Core.Entities.Concrete;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DataAccess.Abstract
 {
     public interface IUserDal : IEntityRepository<User>
     {
+        List<OperationClaim> GetClaims(User user);
     }
 }
--- DataAccess/Concrete/EntitiyFramework/EfUserDal.cs
+++ DataAccess/Concrete/EntitiyFramework/EfUserDal.cs
@@ -1,10 +1,25 @@
 ﻿using Core.DataAccess.EntityFramework;
 using Core.Entities.Concrete;
 using DataAccess.Abstract;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataAccess.Concrete.EntitiyFramework
 {
     public class EfUserDal : EfEntityRepositoryBase<User, ReCapProjectDbContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new ReCapProjectDbContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                             on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+
+                return result.ToList();
+            }
+        }
     }
 }
