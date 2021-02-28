7771e2239313359a1b0da55a0e0e0a3866da6480
YusufAkkurt <ysfakkurt@hotmail.com>
2021-02-28 17:03:00 +0300

    Bug fixed

--- Business/Concrete/AuthManager.cs
+++ Business/Concrete/AuthManager.cs
@@ -57,7 +57,7 @@ namespace Business.Concrete
 
         public IResult UserExists(string email)
         {
-            if (_userService.GetByMail(email) != null)
+            if (_userService.GetByMail(email).Data != null)
             {
                 return new ErrorResult(Messages.UserAlreadyExists);
             }
