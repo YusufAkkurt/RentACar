95f45843ef8d956e00a6a29bf000eb446436bd35
YusufAkkurt <ysfakkurt@hotmail.com>
2021-02-28 16:12:34 +0300

    User Register ve Login Dto nesneleri eklendi

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities.Concrete;
 using Core.Utilities.Results.Abstract;
+using Core.Utilities.Security.JWT;
 using System.Collections.Generic;
 
 namespace Business.Abstract
@@ -8,9 +9,19 @@ namespace Business.Abstract
     {
         IDataResult<List<User>> GetAll();
         IDataResult<User> GetById(int userId);
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
 
         IResult Add(User user);
         IResult Update(User user);
         IResult Delete(User user);
+    }
+
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
     }
 }
---
+++ Entities/DTOs/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Abstract;
+
+namespace Entities.DTOs
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
